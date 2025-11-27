e8f47d84a2269ae53d346d8250964813e71232c7
Jessica <jessica.lima22@hotmail.com>
2025-11-27 00:40:16 -0300

    about page n routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,70 @@
 import './App.css'
+import aboutVanImage from "../public/about_van.png"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
 
 function App() {
-  
   return (
-    <>
+    <BrowserRouter basename='/van-life'>
       <header>
-        <span>#VANLIFE</span>
+        <span>
+          <Link to="/">#VANLIFE</Link>
+        </span>
         <div>
-          <a className='header_link'>About</a>
-          <a className='header_link'>Vans</a>
+          <Link to="/about" className='header_link'>About</Link>
+          <Link to="/vans" className='header_link'>Vans</Link>
         </div>
       </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      <footer>Ⓒ 2026 #VANLIFE</footer>
+    </BrowserRouter>
+  )
+}
 
+function Home() {
+  return (
+    <>
       <main>
-        <div className='main_content'>
-          <h1>You got the travel plans, we got the travel vans.</h1>
-          <p>Add adventure to your life by joining the #vanlife movement.</p>
-          <p>Rent the perfect van to make your perfect road trip.</p>
-          <button className='find_your_van'>Find your van</button>
+        <div className='main_home'>
+          <div className='main_content'>
+            <h1>You got the travel plans, we got the travel vans.</h1>
+            <p>Add adventure to your life by joining the #vanlife movement.</p>
+            <p>Rent the perfect van to make your perfect road trip.</p>
+            <button className='find_your_van_button'>Find your van</button>
+          </div>
         </div>
       </main>
-      <footer>Ⓒ 2022 #VANLIFE</footer>
     </>
+  )
+}
+
+function About (){
+  return (
+    <>
+    <main>
+      <img src={aboutVanImage} alt="Image of a Van" className='about_image'/>
+      <div className='main_about'>
+        <div className='main_about_content'>
+          <h2>Don’t squeeze in a sedan when you could relax in a van.</h2>
+          <p> 
+            Our mission is to enliven your road trip with the perfect travel van rental. Our vans are recertified before each trip to ensure your travel plans can go off without a hitch.
+            (Hitch costs extra 😉)
+          </p> 
+          <p> 
+            Our team is full of vanlife enthusiasts who know firsthand the magic of touring the world on 4 wheels.
+          </p>
+          <div className='about_down'>
+            <h3>Your destination is waiting.</h3>
+            <h3>Your van is ready.</h3>
+            <Link to="/vans" className='about_down_button'>Explore our vans</Link>
+          </div>
+        </div>
+      </div>
+    </main>
+    </>
+    
   )
 }
 
